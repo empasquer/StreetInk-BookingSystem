675ad339afb96907c2d7e109d35e0b200c8c1016
emma673r <90177139+emma673r@users.noreply.github.com>
2024-05-08 12:39:53 +0200

    emmas branch, virker det?

--- src/main/java/com/example/streetinkbookingsystem/services/DashboardService.java
+++ src/main/java/com/example/streetinkbookingsystem/services/DashboardService.java
@@ -1,5 +1,12 @@
 package com.example.streetinkbookingsystem.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DashboardService {
 
+    // blabla skal ikke bruges, vil bare teste den nye branch
+    public int calculateStuff(){
+        return 0;
+    }
 }
