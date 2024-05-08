75919454302c32bcb081509bc64267e477ea8b90
emma673r <90177139+emma673r@users.noreply.github.com>
2024-05-08 13:39:10 +0200

    beginning of html and styles - no logic

--- src/main/java/com/example/streetinkbookingsystem/controllers/DashboardController.java
+++ src/main/java/com/example/streetinkbookingsystem/controllers/DashboardController.java
@@ -1,13 +1,22 @@
 package com.example.streetinkbookingsystem.controllers;
 
+import com.example.streetinkbookingsystem.models.TattooArtist;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public String seeDashboard() {
+    public String seeDashboard(Model model) {
+
+        // temporary profile
+        TattooArtist profile = new TattooArtist();
+        profile.setUsername("EmTheBest");
+
+
+        model.addAttribute("profile", profile);
         return "home/dashboard";
     }
 }
