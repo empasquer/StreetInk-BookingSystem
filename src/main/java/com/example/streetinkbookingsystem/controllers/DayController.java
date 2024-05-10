04ad41ba9c7d4ac3c16d25673e99258ae9e8e972
Nanna Petersen <nannahofgaard@hotmail.com>
2024-05-10 15:14:32 +0200

    ss

--- src/main/java/com/example/streetinkbookingsystem/controllers/DayController.java
+++ src/main/java/com/example/streetinkbookingsystem/controllers/DayController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,18 +19,30 @@ public class DayController {
 
     @Autowired
     BookingService bookingService;
-
     @GetMapping("/day")
     public String seeDay(Model model, @RequestParam LocalDate date, @RequestParam(required = false) String username){
         if (username == null){
-            //        HttpSession session = get session, if session is null then redirect to index.
-            // else set username to session.getAttribute(username);
-            return "redirect:/";}
+            // Redirect logic when username is null
+            return "redirect:/";
+        }
 
         List<Booking> bookingList = bookingService.getBookingsForDay(date,username);
         model.addAttribute("bookingList", bookingList);
         model.addAttribute("date", date);
+        model.addAttribute("username", username);
         return "home/day";
+    }
+
+    @PostMapping("/day/next")
+    public String seeNextDay( @RequestParam LocalDate date, @RequestParam String username) {
+        LocalDate nextDate = date.plusDays(1);
+        return "redirect:/day?username=" + username + "&date=" + nextDate;
+    }
+
+    @PostMapping("/day/previous")
+    public String seePreviousDay( @RequestParam LocalDate date,@RequestParam String username) {
+        LocalDate previousDate = date.minusDays(1);
+        return "redirect:/day?username=" + username + "&date=" + previousDate;
     }
 
 
