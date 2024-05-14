c2c9723449c54484c7229c2a7a2a3307729eb4b6
BlablaTara <tara0561@stud.kea.dk>
2024-05-14 13:12:56 +0200

    updated index OG bookings

---
+++ src/test/java/com/example/streetinkbookingsystem/BookingServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.streetinkbookingsystem;
+
+import com.example.streetinkbookingsystem.services.BookingService;
+import org.junit.jupiter.api.Test;
+
+public class BookingServiceTest {
+    BookingService bookingService;
+
+    @Test
+    public void givingBookingId_ReturningBookingInformation(){
+
+    }
+}
